e8e96bc62e2736fd75d984be75204078ccfe005a
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2018-07-02 19:20:02 -0300

    Upload photo php

--- include/functions.php
+++ include/functions.php
@@ -3,26 +3,23 @@
 function uploadImg(){
 			// ENVIA IMAGEM
 			$target_dir = "img/noticia/"; // MUDA DIRETÓRIO
-			$target_file = $target_dir . basename($_FILES["imagem"]["name"]);
-			$uploadOk = 1;
-		
-			
-			$extension = strtolower(pathinfo($_FILES["imagem"]["name"] ,PATHINFO_EXTENSION));
-			$newName = md5(uniqid("")).".".$extension;
+			$target_file = $target_dir . basename($_FILES["imagem"]["name"]); // PEGA O ARQUIVO
+			$uploadOk = 1; // 1 QUER DIZER OK
+			$extension = strtolower(pathinfo($_FILES["imagem"]["name"] ,PATHINFO_EXTENSION)); // PEGA EXTENSÃO
+			$newName = md5(uniqid("")).".".$extension; // GERA NOME ALEATORIO COM A EXTENSÃO
 			
 			
 			// VERIFICA SE IMAGEM É JPG, PNG OU GIR
 			if($extension != "jpg" && $extension != "png" && $extension != "jpeg"
 				&& $extension != "gif" ) {
-					echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
 					$uploadOk = 0;
 			}
+			
+			// SE estiver ok, $uploadOk =1, move o arquivo
 			if ($uploadOk == 0) {
 				$msg = "Error de Upload";
-			// if everything is ok, try to upload file
 			} else {
 				if (move_uploaded_file($_FILES["imagem"]["tmp_name"], $target_dir.$newName)) {
-					
 				} else {
 					$msg = "Error de Upload";
 				}
