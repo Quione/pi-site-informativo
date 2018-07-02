9ebd8ff12538a515ecefb70da5b082c0b396cd60
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2018-07-02 19:17:40 -0300

    Upload photo php

--- include/functions.php
+++ include/functions.php
@@ -5,15 +5,15 @@ function uploadImg(){
 			$target_dir = "img/noticia/"; // MUDA DIRETÓRIO
 			$target_file = $target_dir . basename($_FILES["imagem"]["name"]);
 			$uploadOk = 1;
-			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		
 			
 			$extension = strtolower(pathinfo($_FILES["imagem"]["name"] ,PATHINFO_EXTENSION));
 			$newName = md5(uniqid("")).".".$extension;
 			
 			
 			// VERIFICA SE IMAGEM É JPG, PNG OU GIR
-			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-				&& $imageFileType != "gif" ) {
+			if($extension != "jpg" && $extension != "png" && $extension != "jpeg"
+				&& $extension != "gif" ) {
 					echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
 					$uploadOk = 0;
 			}
